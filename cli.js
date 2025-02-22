42f07c697cb8a614f60abbf7e52c9d4af7bf6382
Alan Shaw <alan@storacha.network>
2025-02-22 08:59:51 +0000

    fix: use github direct

--- cli.js
+++ cli.js
@@ -1,8 +1,6 @@
 import sade from 'sade'
-import { Magic } from 'magic-sdk'
-import { OAuthExtension } from '@magic-ext/oauth2'
 
-const MAGIC_PUBLISHABLE_API_KEY = 'pk_live_8A718F43F8299E28'
+const githubOAuthClientID = 'Ov23liRdyizj8EndxxAf'
 
 const cli = sade('w3')
 
@@ -10,23 +8,11 @@ cli.version('0.0.0')
 
 cli
   .command('login')
-  .option('--github', 'Login with GitHub')
+  .option('--github', 'Login with GitHub', false)
   .describe('Authenticate this agent with your email address to gain access to all capabilities that have been delegated to it.')
   .action(async () => {
-    const magic = new Magic(MAGIC_PUBLISHABLE_API_KEY, {
-      extensions: [new OAuthExtension()]
-    })
-    
-    await magic.oauth2.loginWithRedirect({
-      provider: 'github',
-      redirectURI: 'https://alantest.exmaple.com'
-    })
-
-    const isLoggedIn = await magic.user.isLoggedIn()
-    if (!isLoggedIn) {
-      throw new Error('not logged in')
-    }
-
-    console.log('ID token: ', await magic.user.getIdToken())
-    console.log('Info: ', await magic.user.getInfo())
+    console.log('Click the following link to authenticate with github:')
+    console.log(`https://github.com/login/oauth/authorize?scope=user&client_id=${githubOAuthClientID}`)
   })
+
+cli.parse(process.argv)
--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 import http from 'node:http'
 
-const server = http.createServer((req, res) => {
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
+
+const server = http.createServer(async (req, res) => {
   console.log(req.url)
+  const url = new URL(req.url, 'http://localhost')
+  if (url.pathname === '/callback') {
+    const code = url.searchParams.get('code')
+    if (!code) {
+      res.statusCode = 400
+      return res.end()
+    }
+    const params = new FormData()
+    params.set('client_id', GITHUB_CLIENT_ID)
+    params.set('client_secret', GITHUB_CLIENT_SECRET)
+    params.set('code', code)
+    const res = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: { Accept: 'application/json' },
+      body: params
+    })
+    console.log(await res.json())
+  }
   res.end()
 })
 
